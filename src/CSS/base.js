98986eaa57b4b574cd1f89a445ba34573d6db5cf
Emiliano Fernández <emilianofh02@gmail.com>
2023-06-11 21:14:49 -0700

    first push

---
+++ src/CSS/base.js
@@ -0,0 +1,13 @@
+import { css } from "../JS/Lit.js";
+
+export const base = css`
+  :root {
+    font-size: 9px;
+    font-family: "Montserrat";
+  }
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`;
